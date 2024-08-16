685656d94e329ca20d75b4bba6f68fa93b5520fd
Ajitesh Kumar <ajinest6@gmail.com>
2024-08-16 11:04:05 -0400

    static

--- server.go
+++ server.go
@@ -8,11 +8,21 @@ import (
 	"github.com/tiredkangaroo/sculpt"
 )
 
+type AssetsInput struct {
+	FileName string `kind:"path"`
+}
+
 func startServer(mode string, item *sculpt.Model) {
 	app := puff.DefaultApp("Checklist")
 
 	fFR := puff.FileResponse{FilePath: "static/index.html"}
 	app.Get("/", "Serves static application.", nil, fFR.Handler())
+
+	assetsInput := new(AssetsInput)
+	app.Get("/assets/{file}", "Serves assets for static application.", assetsInput, func(c *puff.Context) {
+		// FIXME: potentially dangerous
+		c.SendResponse(puff.FileResponse{FilePath: "static/assets/" + assetsInput.FileName})
+	})
 
 	apiRouter := getAPIRouter(item)
 
